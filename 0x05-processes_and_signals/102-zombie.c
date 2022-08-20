af161336801c4c4cce94f581ea531365a2f6988e
Maurice <mauricejahwizwid@gmail.com>
2022-08-20 03:52:37 +0100

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,2 +1,29 @@
-#!/usr/bin/env bash
-# This script INSERT DESCRIPTION HERE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * main - zombies
+ *
+ * Description: make five zombies
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+	pid_t pidme;
+
+	i = 0;
+	while (i < 5)
+	{
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
+	}
+	sleep(100);
+	return (0);
+}
